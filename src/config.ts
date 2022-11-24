86af9c65df6ee68c986233b758cbf83d6875e3f7
dimas <dimaslex@gmail.com>
2022-11-25 00:21:08 +0100

    Init config

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+//@ts-ignore
+window._config = {
+  cognito: {
+      userPoolId: 'us-east-1_d80kgdwB0', // e.g. us-east-2_uXboG5pAb
+      userPoolClientId: '6hh49sgt0ps0810ksh5gh95m34', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+      region: 'us-west-2' // e.g. us-east-2
+  },
+  api: {
+      invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  }
+};
